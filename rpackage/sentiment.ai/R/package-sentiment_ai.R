819e6f87da9b0cccce307530e0097aa5a0c64965
Steven Nydick <steven.nydick@kornferry.com>
2021-05-04 16:42:24 -0500

    Fix infrastructure using usethis, update description, add readme/NEWS, update license

---
+++ rpackage/sentiment.ai/R/package-sentiment_ai.R
@@ -0,0 +1,22 @@
+#' @description
+#' This package uses Google's Universal Sentence Encoder, simplifies all the
+#' difficulties, and turns it into a sentiment analysis package.
+#'
+#' Main Benefits:
+#' \itemize{
+#'   \item{Tolerates spelling mistakes}
+#'   \item{Not dependent on exactly matching a fixed dictionary}
+#'   \item{Requires less pre-processing}
+#'   \item{More powerful than dictionary-based methods}
+#' }
+#'
+#' Main Drawbacks:
+#' \itemize{
+#'   \item{Requires a lot of RAM}
+#'   \item{Can be slow on larger datasets (unless using GPU)}
+#' }
+#'
+#' Effectivelly, if you have a reasonably powerful computer, you can use
+#' sentiment.ai as a more flexible, powerful, and modern approach to sentiment
+#' analysis.
+"_PACKAGE"
