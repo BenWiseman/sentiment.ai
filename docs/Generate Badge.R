a855c770cf3c60674419cf4683e6e2de27617f4a
BenWiseman <benjamin.h.wiseman@gmail.com>
2021-12-14 00:10:15 -0800

    description

--- docs/Generate Badge.R
+++ docs/Generate Badge.R
@@ -7,9 +7,9 @@ font_add_google("Nanum Gothic Coding", "nanum")
 showtext_auto()
 
 sticker(imgurl, package="sentiment.ai",
-        p_size=7, p_x=1, p_y=1.05,
+        p_size=20, p_x=1, p_y=1.05,
         p_color = "#09001c", p_family = "nanum",
         s_x=1, s_y=1, s_height = .6, s_width = .6,
-        h_color = "#EEACD4", h_fill = "#ffffff",
+        h_color = "#3378c4", h_fill = "#ffffff",
         spotlight = FALSE, l_x=1, l_y=1, l_alpha = .2,
         filename="inst/figures/sentiment.ai.png")
